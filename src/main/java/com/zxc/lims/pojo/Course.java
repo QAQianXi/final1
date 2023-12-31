475978c0cec8acfc187b3b74b8c31bee105abe11
QianXi <qianxi.study@outlook.com>
2023-12-31 15:21:29 +0800

    4

---
+++ src/main/java/com/zxc/lims/pojo/Course.java
@@ -0,0 +1,38 @@
+package com.zxc.lims.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * Description 课程信息实体类
+ * Author: zxc
+ * Date: 2020/3/11 14:50
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+  private String id;
+  private String name;
+  private Double credits;
+  private Integer score;
+  private Integer number;
+  private Integer year;
+  private Integer term;
+  private Integer type;
+  private String profession;
+  private String grade;
+  private String realName;
+  private String no;
+  private String studentId;
+  private String pointByUser;
+  private String scoreByUser;
+  private String creditsByUser;
+  private Integer start;
+  private Integer end;
+  private String room;
+  private Date createTime;
+}
---
+++ src/main/java/com/zxc/lims/pojo/Score.java
@@ -0,0 +1,29 @@
+package com.zxc.lims.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Description 分数信息实体类
+ * Author: zxc
+ * Date: 2020/3/19 10:55
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Score {
+  private Long id;
+  private String username;
+  private String name;
+  private String scoreByUser;
+  private String pointByUser;
+  private String creditsByUser;
+  private Integer score;
+  private String credits;
+  private Integer term;
+  private Integer year;
+  private String no;
+  private String studentId;
+  private String courseId;
+}
